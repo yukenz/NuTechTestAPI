13520c9210ae11dba8ab80e811f2b8e1faaaf9e9
yukenz <yuyun.purniawan@gmail.com>
2023-10-27 17:46:02 +0700

    Fix : Missconfiguration Equality and Annotation

--- src/main/java/nutech/awan/ppob/CustomWebMvcConfigure.java
+++ src/main/java/nutech/awan/ppob/CustomWebMvcConfigure.java
@@ -2,13 +2,13 @@ package nutech.awan.ppob;
 
 import nutech.awan.ppob.resolver.MemberArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@Component
+@Configuration
 public class CustomWebMvcConfigure implements WebMvcConfigurer {
 
     @Autowired
--- src/main/java/nutech/awan/ppob/resolver/MemberArgumentResolver.java
+++ src/main/java/nutech/awan/ppob/resolver/MemberArgumentResolver.java
@@ -3,14 +3,17 @@ package nutech.awan.ppob.resolver;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import nutech.awan.ppob.model.entity.Member;
+import nutech.awan.ppob.repository.interfaces.MemberRepository;
 import nutech.awan.ppob.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
@@ -18,9 +21,12 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
     @Autowired
     JWTUtil jwtUtil;
 
+    @Autowired
+    MemberRepository memberRepository;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.equals(Member.class);
+        return parameter.getParameterType().equals(Member.class);
     }
 
     @Override
@@ -32,8 +38,9 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
         Claims claims = jwtUtil.parseBearerToken(token);
         String email = claims.getSubject();
 
+        Member member = memberRepository.findById(email)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Jwt benar namun di DB tidak ada email tersebut"));
 
-
-        return null;
+        return member;
     }
 }

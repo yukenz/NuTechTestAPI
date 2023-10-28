ce78c96d2b352150e17ceee3aa86fa29082714e0
yukenz <yuyun.purniawan@gmail.com>
2023-10-28 12:10:52 +0700

    Fix : Naming InformationService

--- src/main/java/nutech/awan/ppob/service/interfaces/InformationService.java
+++ src/main/java/nutech/awan/ppob/service/interfaces/InformationService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public interface InfomationService {
+public interface InformationService {
 
     List<ListBannerResponse> banner();
 

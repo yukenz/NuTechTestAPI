585d63eddd79fa0deb97023160478064f1d9947b
yukenz <yuyun.purniawan@gmail.com>
2023-10-29 09:41:59 +0700

    Refactor : Database Properties to app prop

--- src/main/java/nutech/awan/ppob/utils/CustomDataSource.java
+++ src/main/java/nutech/awan/ppob/utils/CustomDataSource.java
@@ -2,6 +2,7 @@ package nutech.awan.ppob.utils;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,14 +11,23 @@ import java.time.Duration;
 @Configuration
 public class CustomDataSource {
 
+    @Value("${database.url}")
+    private String JDBCUrl;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
     @Bean
     HikariDataSource hikariDataSource() {
 
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
-        hikariConfig.setJdbcUrl("jdbc:mariadb://localhost:3306/nutech");
-        hikariConfig.setUsername("yukenz");
-        hikariConfig.setPassword("awan");
+        hikariConfig.setJdbcUrl(JDBCUrl);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
 
 
         hikariConfig.setMaximumPoolSize(20);
